6c1eb12fd8a3b756d0009c4902d45be8f15b45e9
Henry Hollis <henryhollis4@gmail.com>
2024-05-31 10:21:58 -0400

    Started adding functionality to keep track of cell identity

--- include/ccd_utils.h
+++ include/ccd_utils.h
@@ -5,6 +5,8 @@
 #ifndef LEIDEN_CCD_CCD_UTILS_H
 #define LEIDEN_CCD_CCD_UTILS_H
 #include <vector>
+#include <unordered_map>
+
 class ccd_utils {
 public:
     static std::vector<double> sliceColumns(const std::vector<double> &matrix, const std::vector<size_t> &columnsToAccess,
@@ -21,5 +23,6 @@ public:
 
     static double cor(const std::vector<double> &X, const std::vector<double> &Y);
 
-};
+    static void sumColumnsByGroup(const std::vector<double>& matrix, size_t rows, size_t cols, const std::vector<size_t>& membership, std::vector<double>& result);
+    };
 #endif //LEIDEN_CCD_CCD_UTILS_H
